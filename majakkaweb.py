fb13dc5cabf7b951c34cb6f5a27f1828405a63a3
Miika Laaksonen <miika.laaksonen@polea.fi>
2021-11-21 19:50:51 +0200

    webpohja majakalle 2

--- majakkaweb.py
+++ majakkaweb.py
@@ -1,31 +1,79 @@
-# Import server module
-import http.server
-
-# Import SocketServer module
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.parse import urlparse
 import socketserver
-
-# Import sys module
+import socket
 import sys
+import fcntl
+import struct
 
-try:
-    # Set the port number
-    if sys.argv[1:]:
-        port = int(sys.argv[1])
-    else:
-        port = 5000
 
-    # Set the IP address
-    server_address = ('127.0.0.1', port)
+class GetHandler(BaseHTTPRequestHandler):
 
-    # Create object for handling HTTP requests
-    Handler = http.server.SimpleHTTPRequestHandler
+    def do_GET(self):
 
-    # Run the web server forever to handle the HTTP requests
-    with socketserver.TCPServer(("", port), Handler) as httpd:
-        print("Web Server is running at http://localhost:%s" %port)
-        httpd.serve_forever()
+        parsed_path = urlparse(self.path)
+        message_parts = [
+            'CLIENT VALUES:',
+            'client_address=%s (%s)' % (self.client_address,
+                                        self.address_string()),
+            'command=%s' % self.command,
+            'path=%s' % self.path,
+            'real path=%s' % parsed_path.path,
+            'query=%s' % parsed_path.query,
+            'request_version=%s' % self.request_version,
+            '',
+            'SERVER VALUES:',
+            'server_version=%s' % self.server_version,
+            'sys_version=%s' % self.sys_version,
+            'protocol_version=%s' % self.protocol_version,
+            '',
+            'HEADERS RECEIVED:',
+        ]
+        for name, value in sorted(self.headers.items()):
+            message_parts.append('%s=%s' % (name, value.rstrip()))
+        message_parts.append('')
+        message = '\r\n'.join(message_parts)
+        print(message)
 
-# Stopped the server
-except KeyboardInterrupt:
-    httpd.server_close()
-    print("The server is stopped.")
+        polku = parsed_path.path
+
+        if polku == "/":
+            f = open('index.html', 'r')
+            message = f.read()
+
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(bytes(message, 'utf-8'))
+        return
+
+
+class IpHandler:
+    def get_ip_address(self, ifname):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s', bytes(ifname[:15], 'utf-8')))[20:24])
+
+
+if __name__ == '__main__':
+
+    try:
+        # Set the port number
+        if sys.argv[1:]:
+            port = int(sys.argv[1])
+        else:
+            port = 80
+
+        local_ip = IpHandler().get_ip_address('wlan0')
+
+        with socketserver.TCPServer(("", port), GetHandler) as httpd:
+            osoite = "http://"+local_ip
+
+            if port != 80:
+                osoite = osoite + ":" + str(port)
+
+            print("Majakka-palvelin osoitteessa " + osoite)
+            httpd.serve_forever()
+
+    # Stopped the server
+    except KeyboardInterrupt:
+        httpd.server_close()
+        print("Majakka-palvelin sammutettu.")
